f3b143ad072e0c797733d5e3d02e1eb1f90d5b4e
GGang <eun1018bee@naver.com>
2024-09-23 22:51:47 +0900

    [API] 공공 데이터 불러오기

--- src/pages/InfomationList.tsx
+++ src/pages/InfomationList.tsx
@@ -5,32 +5,35 @@ export default function InformationList() {
 
   useEffect(() => {
     fetch(
-      "",
+      "https://api.odcloud.kr/api/15089250/v1/uddi:8ac852eb-4b3f-470c-8342-bbcc701e0b8b?page=1&perPage=126&serviceKey=" +
+        process.env.REACT_APP_API_KEY,
       {
         method: "GET",
       }
     )
       .then((res) => res.json())
       .then((data) => {
-        // setList(
-        //   data.data.filter(
-        //     (jsonData:JSON) => jsonData["체육시설현황"].indexOf("테니스") !== -1
-        //   )
-        // );
-        setList(data.data)
+
+        setList(
+          data.data.filter(
+            (jsonData:any) => jsonData["체육시설현황"].indexOf("테니스") !== -1
+          )
+        );
+
       })
       .catch((err) => {
         console.error(err);
       });
   }, []);
 
-  function testFunc() {
-    console.log(list);
-  }
-
   return (
     <>
-      <p onClick={testFunc}>클릭</p>
+      {list.map((item, index) => (
+        <div key={index}>
+          {item["동별"]} {item["시설명"]}
+          <br />
+        </div>
+      ))}
     </>
   );
 }
